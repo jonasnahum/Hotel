c808445d7f33c5c68630867713e2a98d904d0e71
jonasnahum <jonasnahum@gmail.com>
2016-06-06 13:59:12 -0500

    con angular y bootstrap

---
+++ web/public/angular/src/app.js
@@ -0,0 +1,8 @@
+(function () {
+    var app = angular.module('app', ['ngRoute']);
+
+    app.config(["$routeProvider", function ($router) {
+        $router.when("/", { templateUrl: "angular/views/index.html" })
+        .otherwise({ redirectTo: "/" });
+    }]);
+})();
