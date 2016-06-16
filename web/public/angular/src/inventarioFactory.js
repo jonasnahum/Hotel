282606993ac64ba4bfbea33bc7e5ce531b27823e
jonasnahum <jonasnahum@gmail.com>
2016-06-16 10:30:34 -0500

    alert fechas hacia el futuro

--- web/public/angular/src/inventarioFactory.js
+++ web/public/angular/src/inventarioFactory.js
@@ -19,65 +19,8 @@
               {habitacion:13,tipo:"doble", reservaciones: []}
             ];
         };
-        InventarioClass.prototype.filtrarInventariodeHabsPorTipo = function (model){
-          var that = this;
-          var arr = [];
-          for (var i = 0; i < that.inventario.length; i++) {
-            if(that.inventario[i].tipo === model.tipo ) {
-                arr.push(that.inventario[i]);
-            }
-          }
-          return arr;
-        };
-        InventarioClass.prototype.filtrarArrPorFecha = function (arr, model){
-          var that = this;
-          var arr = arr;
-          var filtradas = [];
-          for (var i = 0; i < arr.length; i++) {
-              var entradaDisponible = that._checkarDisponibilidadFechaEntradaDeseada(arr[i],model.fechaEntrada);
-              var salidaDisponible = that._checkarDisponibilidadFechaSalidaDeseada(arr[i],model.fechaSalida);
-              if(entradaDisponible === true && salidaDisponible === true){
-                filtradas.push(arr[i]);
-              }
-          }
-          return filtradas;
-        };
-        InventarioClass.prototype.filtrarPorCantidad = function (arr, model) {
-          var arr = arr;
-          var cuantas = model.habitaciones;
-          var filtradas = [];
-          contador = 0;
 
-          for (var i = 0; i < arr.length; i++) {
-            if(contador<cuantas){
-              filtradas.push(arr[i]);
-              contador++
-            }
-          }
-          return filtradas;
-        };
-
-        InventarioClass.prototype.filtrarLasQueNoAbarcan = function(fechaLibre_tipo, model) {
-          var that = this;
-          var arr = fechaLibre_tipo;
-          var habitacioinesParaBorrar= [];
-
-          for (var i = 0; i < arr.length; i++) {
-            var habitacionDisponible = arr[i];
-            for (var j = 0; j < habitacionDisponible.reservaciones.length; j++) {
-              var reservacion = habitacionDisponible.reservaciones[j];
-              if( model.fechaEntrada < reservacion.fechaEntrada  &&  model.fechaSalida > reservacion.fechaSalida ) {
-                //arr.splice(i,1);
-                habitacioinesParaBorrar.push(habitacionDisponible);
-              }
-            };
-          };
-          if(habitacioinesParaBorrar.length === 0){
-            return arr;
-          }
-          return that.borrarIndicesFromArray(arr, habitacioinesParaBorrar);
-        };
-        InventarioClass.prototype.borrarIndicesFromArray = function(arr,habitacioinesParaBorrar){
+        InventarioClass.prototype._borrarIndicesFromArray = function(arr,habitacioinesParaBorrar){
           var arr = arr;
           for (var j = 0; j < habitacioinesParaBorrar.length; j++) {
             var index = arr.indexOf(habitacioinesParaBorrar[j]);
@@ -87,71 +30,6 @@
           }
           return arr;
         };
-
-
-      /*    var newArr = [];
-          for (var i = 0; i < arr.length; i++) {
-            var habDisponible = arr[i];
-            for (var j = 0; j < habitacioinesParaBorrar.length; j++) {
-              var habBorrar = habitacioinesParaBorrar[j];
-              if(habDisponible != habBorrar){
-                newArr.push(arr[i]);
-              }
-              continue;
-            }
-          }
-          return newArr;
-        };*/
-        InventarioClass.prototype.guardarFiltradas = function(filtradas, model){
-          var that = this;
-          var filtradas = filtradas;
-
-          for (var i = 0; i < filtradas.length; i++) {
-            var habDisponible = filtradas[i];
-            for (var j = 0; j < that.inventario.length; j++) {
-              var habInventario = that.inventario[j];
-              if(habDisponible === habInventario){
-                  habInventario.reservaciones.push({fechaEntrada:model.fechaEntrada,fechaSalida:model.fechaSalida});
-                  continue;
-              }
-            }
-          };
-          return;
-        };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        InventarioClass.prototype.quitarNoDeseadasDeArr = function (arr, noDeseadas){
-          var arr = arr;
-          var noDeseadas = noDeseadas;
-          for (var i = 0; i < arr.length; i++) {
-            hab = arr[i];
-            for (var j = 0; j < noDeseadas.length; j++) {
-              var habToDelete = noDeseadas[j];
-              if(hab === habToDelete){
-                  //borrar hab de arr
-                  arr.splice(i,1);
-              }
-            }
-          }
-          return arr;
-        };
-
-
-
         InventarioClass.prototype._checkarDisponibilidadFechaEntradaDeseada = function (habitacion,fechaEntrada) {
           var that = this;
           var disponible = true;
@@ -179,73 +57,77 @@
           return disponible;
         };
 
-        InventarioClass.prototype._habitacionAbarcaFechasRegistradas = function(entrada,salida) {
-          var that = this;
-          for (var i = 0; i < that.inventario.length; i++) {
-            if(that.inventario[i].reservaciones.length === 0 ){
-              //habitacion no abarca fechas registradas
-              continue;
-            };
-            if(that.inventario[i].reservaciones.fechaEntrada > entrada && that.inventario[i].reservaciones.fechaSalida < salida  ) {
-              //habitacion si abarca fechas registradas
-                return true;
-            }
-          };
-          //no hay inventario.
-          return false;
-        };
 
-
-        InventarioClass.prototype._sonIguales = function (entrada, salida) {
-          var that = this;
-          if(entrada === true && salida === true){
-            return true
-          }
-          return false;
-        };
-
-        InventarioClass.prototype._conseguirArrHabitacionesDisponiblesSegunTipoYFecha = function (model){
+        InventarioClass.prototype.filtrarInventariodeHabsPorTipo = function (model){
           var that = this;
           var arr = [];
           for (var i = 0; i < that.inventario.length; i++) {
             if(that.inventario[i].tipo === model.tipo ) {
-              var entradaDisponible = that._checkarDisponibilidadFechaEntradaDeseada(that.inventario[i],model.fechaEntrada);
-              var salidaDisponible = that._checkarDisponibilidadFechaSalidaDeseada(that.inventario[i],model.fechaSalida);
-              var abarca = that._habitacionAbarcaFechasRegistradas(model.fechaEntrada,model.fechaSalida);
-              if(entradaDisponible === true && salidaDisponible === true && abarca === false){
                 arr.push(that.inventario[i]);
-              }
             }
           }
           return arr;
         };
-/*
-       InventarioClass.prototype.checkarDisponibilidadDeHabitaciones = function(model){
-          var that = this;
-          var arrDisponibles = that._conseguirArrHabitacionesDisponiblesSegunTipoYFecha(model);
-          var cuantasQuieren = model.habitaciones;
-          var cantidadHabitacionesDisponibles = arrDisponibles.length;
-          if(cantidadHabitacionesDisponibles >= cuantasQuieren){
-            return true;
-          }
-          return false;
-        };
-*/
-        InventarioClass.prototype.guardarReservacionEnInventario = function(model,filtradas){
-          var that = this;
-          var cuantas = model.habitaciones;
-          //arrDisponibles >= cuantas, y son del tipo deseado
-          var arrDisponibles = filtradas;//that._conseguirArrHabitacionesDisponiblesSegunTipoYFecha(model);
-          var inventario = that.inventario;
 
-          var contadorDeReservacionesGuardadasEnInventario = 0;
-          for (var i = 0; i < arrDisponibles.length; i++) {
-            var habDisponible = arrDisponibles[i];
-            for (var j = 0; j < inventario.length; j++) {
-              var habInventario = inventario[j];
-              if(habDisponible === habInventario && contadorDeReservacionesGuardadasEnInventario < cuantas){
+        InventarioClass.prototype.filtrarArrPorFecha = function (arr, model){
+          var that = this;
+          var arr = arr;
+          var filtradas = [];
+          for (var i = 0; i < arr.length; i++) {
+              var entradaDisponible = that._checkarDisponibilidadFechaEntradaDeseada(arr[i],model.fechaEntrada);
+              var salidaDisponible = that._checkarDisponibilidadFechaSalidaDeseada(arr[i],model.fechaSalida);
+              if(entradaDisponible === true && salidaDisponible === true){
+                filtradas.push(arr[i]);
+              }
+          }
+          return filtradas;
+        };
+
+        InventarioClass.prototype.filtrarPorCantidad = function (arr, model) {
+          var arr = arr;
+          var cuantas = model.habitaciones;
+          var filtradas = [];
+          contador = 0;
+
+          for (var i = 0; i < arr.length; i++) {
+            if(contador<cuantas){
+              filtradas.push(arr[i]);
+              contador++
+            }
+          }
+          return filtradas;
+        };
+
+        InventarioClass.prototype.filtrarLasQueNoAbarcan = function(fechaLibre_tipo, model) {
+          var that = this;
+          var arr = fechaLibre_tipo;
+          var habitacioinesParaBorrar= [];
+
+          for (var i = 0; i < arr.length; i++) {
+            var habitacionDisponible = arr[i];
+            for (var j = 0; j < habitacionDisponible.reservaciones.length; j++) {
+              var reservacion = habitacionDisponible.reservaciones[j];
+              if( model.fechaEntrada < reservacion.fechaEntrada  &&  model.fechaSalida > reservacion.fechaSalida ) {
+                habitacioinesParaBorrar.push(habitacionDisponible);
+              }
+            };
+          };
+          if(habitacioinesParaBorrar.length === 0){
+            return arr;
+          }
+          return that._borrarIndicesFromArray(arr, habitacioinesParaBorrar);
+        };
+
+        InventarioClass.prototype.guardarFiltradas = function(filtradas, model){
+          var that = this;
+          var filtradas = filtradas;
+
+          for (var i = 0; i < filtradas.length; i++) {
+            var habDisponible = filtradas[i];
+            for (var j = 0; j < that.inventario.length; j++) {
+              var habInventario = that.inventario[j];
+              if(habDisponible === habInventario){
                   habInventario.reservaciones.push({fechaEntrada:model.fechaEntrada,fechaSalida:model.fechaSalida});
-                  contadorDeReservacionesGuardadasEnInventario ++;
                   continue;
               }
             }
--- web/public/angular/src/registroController.js
+++ web/public/angular/src/registroController.js
@@ -40,8 +40,56 @@
           return contador;
         };
 
+        ctrl.fechasCongruentes = function functionName(fechaEntrada, fechaSalida) {
+          if(fechaEntrada < fechaSalida){
+            return true;
+          }
+          return false;
+        };
+        ctrl.getCurrentDate = function (){
+          var today = new Date();
+          var dd = today.getDate();
+          var mm = today.getMonth()+1; //January is 0!
+          var yyyy = today.getFullYear();
+
+          if(dd<10) {
+              dd='0'+dd
+          }
+
+          if(mm<10) {
+              mm='0'+mm
+          }
+
+          today = yyyy+'-'+mm+'-'+dd;
+          return today;
+        };
+        ctrl.fechasEnFuturo = function (fechaEntrada, fechaSalida) {
+          var todayString = ctrl.getCurrentDate();
+          var today = new Date(todayString);//entrada y salida deben estar construidas con el mismo formato de string.
+          var entrada = new Date(fechaEntrada);
+          var todayMillisec = today.getTime();
+          var entradaMillisec = entrada.getTime();
+
+          if(todayMillisec <= entradaMillisec){
+              return true;
+          }
+          return false;
+        };
+
         ctrl.guardar = function() {
             var model = ctrl.model;
+
+            var fc = ctrl.fechasCongruentes(model.fechaEntrada, model.fechaSalida);
+            if(fc === false){
+              alert("Error fecha de entrada debe ser menor a la fecha de salida.");
+              return;
+            };
+
+            var ff = ctrl.fechasEnFuturo(model.fechaEntrada, model.fechaSalida);
+            if(ff === false){
+              alert("Error, fechas deben ser a partir de hoy y hacia el futuro.");
+              return;
+            };
 
             var porTipo = ctrl.inventario.filtrarInventariodeHabsPorTipo(model);
             if(porTipo.length === 0){
@@ -51,7 +99,7 @@
 
             var fechaLibre_tipo = ctrl.inventario.filtrarArrPorFecha(porTipo, model);
             if(fechaLibre_tipo.length === 0){
-              alert("Error en fecha");
+              alert("Error, el tipo de habitación y la fecha, se encuentran ocupados.");
               return;
             };
 
@@ -76,7 +124,7 @@
             $( "#fechaEntrada" ).datepicker({
               showWeek: true,
               firstDay: 1,
-              dateFormat: "mm-dd-yy"
+              dateFormat: "yy-mm-dd"//dateFormat: "mm-dd-yy"
             });
             $( "#fechaEntrada" ).change(function(){
                 ctrl.model.fechaEntrada = $(this).val();
@@ -84,7 +132,7 @@
             $( "#fechaSalida" ).datepicker({
               showWeek: true,
               firstDay: 1,
-              dateFormat: "mm-dd-yy"
+              dateFormat: "yy-mm-dd"//dateFormat: "mm-dd-yy"
             });
             $( "#fechaSalida" ).change(function(){
                 ctrl.model.fechaSalida = $(this).val();
