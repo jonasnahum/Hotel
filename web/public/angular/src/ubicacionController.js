f21ea88abd570c4784df29df13d0df4c38eeac26
jonasnahum <jonasnahum@gmail.com>
2016-08-17 14:54:27 -0500

    muestra ruta de un punto a otro

--- web/public/angular/src/ubicacionController.js
+++ web/public/angular/src/ubicacionController.js
@@ -1,3 +1,4 @@
+/*
 (function() {
     var app = angular.module('app');
 
@@ -19,5 +20,50 @@
         }
         initMap();
 
+    }]);
+})();
+*/
+(function() {
+    var app = angular.module('app');
+
+    app.controller('ubicacionController', [ function() {
+        var ctrl = this;
+
+        var directionsDisplay;
+        var directionsService = new google.maps.DirectionsService();
+        var map;
+        var gpsPosition = new google.maps.LatLng(19, -101);
+        var hotelPosition = new google.maps.LatLng(19.4123538, -102.07159817);
+
+        ctrl.initMap = function() {
+            directionsDisplay = new google.maps.DirectionsRenderer();
+            var mapOptions = {
+              zoom: 14,
+              center: hotelPosition
+            }
+            map = new google.maps.Map(document.getElementById("map"), mapOptions);
+            directionsDisplay.setMap(map);
+            var marker = new google.maps.Marker({
+                position: hotelPosition,
+                map: map,
+                title: 'Nombre de mi Hotel.'
+            });
+        }
+        ctrl.calcRoute = function (selectedMode) {
+          var request = {
+              origin: gpsPosition,
+              destination: hotelPosition,
+              // Note that Javascript allows us to access the constant
+              // using square brackets and a string value as its
+              // "property."
+              travelMode: google.maps.TravelMode[selectedMode]
+          };
+          directionsService.route(request, function(response, status) {
+            if (status == google.maps.DirectionsStatus.OK) {
+              directionsDisplay.setDirections(response);
+            }
+          });
+        }
+        ctrl.initMap();
     }]);
 })();
