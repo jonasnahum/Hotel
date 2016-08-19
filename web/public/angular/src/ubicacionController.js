392c53d3562c5e3a7f8fc7f1a17d2ea8a6baffb1
jonasnahum <jonasnahum@gmail.com>
2016-08-19 09:40:21 -0500

    jonas

--- web/public/angular/src/ubicacionController.js
+++ web/public/angular/src/ubicacionController.js
@@ -32,8 +32,10 @@
         var directionsDisplay;
         var directionsService = new google.maps.DirectionsService();
         var map;
-        var gpsPosition = new google.maps.LatLng(19, -101);
+        var myPosition;
         var hotelPosition = new google.maps.LatLng(19.4123538, -102.07159817);
+        var eventListener;
+        var markersArray = [];
 
         ctrl.initMap = function() {
             directionsDisplay = new google.maps.DirectionsRenderer();
@@ -48,10 +50,14 @@
                 map: map,
                 title: 'Nombre de mi Hotel.'
             });
+            var infowindow = new google.maps.InfoWindow({
+                content: 'MI HOTEL'
+            });
+            infowindow.open(map, marker);
         }
         ctrl.calcRoute = function (selectedMode) {
           var request = {
-              origin: gpsPosition,
+              origin: myPosition,
               destination: hotelPosition,
               // Note that Javascript allows us to access the constant
               // using square brackets and a string value as its
@@ -64,6 +70,36 @@
             }
           });
         }
+        ctrl.getEventListener = function(){
+            eventListener = google.maps.event.addListener(map, 'click', function(event){
+                ctrl.manageEvent(event);
+            });
+        };
+        ctrl.manageEvent = function(event){
+            myPosition = new google.maps.LatLng(event.latLng.lat(), event.latLng.lng());
+            ctrl.clearOverlays();
+            //ctrl.placeMarker(event.latLng.lat(), event.latLng.lng());
+            ctrl.calcRoute("DRIVING");
+        };
+        ctrl.clearOverlays = function () {
+            for (var i = 0; i < markersArray.length; i++ ) {
+                markersArray[i].setMap(null);
+            }
+        };
+        ctrl.placeMarker = function (latitud,longitud) {
+            var myMarkerPosition = undefined;
+            myMarkerPosition = new google.maps.LatLng(latitud,longitud);
+            var marker = new google.maps.Marker({
+                position: myMarkerPosition,
+                map: map
+            });
+            markersArray.push(marker);
+            var infowindow = new google.maps.InfoWindow({
+                content: 'Mi ubicación '
+            });
+            infowindow.open(map, marker);
+        };
         ctrl.initMap();
+        ctrl.getEventListener();
     }]);
 })();
