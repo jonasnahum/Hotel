72b8d9b831786974a8ac12e933570218e585777e
jonasnahum <jonasnahum@gmail.com>
2016-08-23 15:47:23 -0500

    barra activa

--- web/public/angular/src/mainController.js
+++ web/public/angular/src/mainController.js
@@ -1,12 +1,18 @@
 (function() {
     var app = angular.module('app');
     var depArr = ['$location', '$anchorScroll'];
-
     depArr.push(function($location,  $anchorScroll) {
         var ctrl = this;
+        ctrl.area = "bienvenida";
         ctrl.scrollTo = function(id) {
            $location.hash(id);
+           ctrl.area = id;
            $anchorScroll();
+        };
+        ctrl.isActive = function(texto) {
+           if(ctrl.area === texto)
+            return true;
+           return false;
         };
     });
     app.controller('mainController', depArr);
